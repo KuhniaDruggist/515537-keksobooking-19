65cedc8c7c458621b6d1b7e028488fc31fe39eb6
Алексей Кухаренко <druggist5059@gmail.com>
2020-02-09 21:26:09 +0300

    adds edits. selects an object from a function

--- js/main.js
+++ js/main.js
@@ -104,44 +104,36 @@ var renderPins = function () {
 
 similarListPin.appendChild(renderPins());
 
+var TypeOfHouse = {
+  flat: 'Квартира',
+  bungalo: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец'
+};
+
 var renderAnnouncementCard = function (card) {
   var cardElement = similarAnnouncementCardTemplate.cloneNode(true);
 
   cardElement.querySelector('.popup__avatar').src = card.author.avatar;
   cardElement.querySelector('.popup__title').textContent = card.offer.title;
   cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
-  if (card.offer.price) {
-    cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
-  } else {
-    cardElement.querySelector('.popup__text--price').textContent = '';
-  }
 
-  var typeHouse = {
-    flat: 'Квартира',
-    bungalo: 'Бунгало',
-    house: 'Дом',
-    palace: 'Дворец'
-  };
+  cardElement.querySelector('.popup__text--price')
+    .textContent = card.offer.price ? card.offer.price + '₽/ночь' : '';
 
-  cardElement.querySelector('.popup__type').textContent = typeHouse[card.offer.type];
+  cardElement.querySelector('.popup__type').textContent = TypeOfHouse[card.offer.type];
 
-  if (card.offer.rooms && card.offer.guests) {
-    cardElement.querySelector('.popup__text--capacity')
-      .textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
-  } else {
-    cardElement.querySelector('.popup__text--capacity').textContent = '';
-  }
+  cardElement.querySelector('.popup__text--capacity')
+    .textContent = (card.offer.rooms && card.offer.guests) ?
+    card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей' : '';
 
-  if (card.offer.checkin && card.offer.checkout) {
-    cardElement.querySelector('.popup__text--time')
-      .textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
-  } else {
-    cardElement.querySelector('.popup__text--time').textContent = '';
-  }
+  cardElement.querySelector('.popup__text--time')
+    .textContent = (card.offer.checkin && card.offer.checkout) ?
+    'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout : '';
 
   if (card.offer.features) {
     for (var i = 0; i < card.offer.features.length; i++) {
-      if (card.offer.features[i]) {
+      if (card.offer.features[i] && card.offer.features.length) {
         cardElement.querySelector('.popup__feature--' + card.offer.features[i]).textContent = card.offer.features[i];
       }
     }
@@ -149,7 +141,7 @@ var renderAnnouncementCard = function (card) {
     cardElement.querySelector('.popup__features').innerHTML = '';
   }
 
-  if (card.offer.photos) {
+  if (card.offer.photos && card.offer.photos.length) {
     cardElement.querySelector('.popup__photos').innerHTML = '';
     var photosList = cardElement.querySelector('.popup__photos');
     for (var j = 0; j < card.offer.photos.length; j++) {
