b5c0b7bacaea883680b9ddb1e7e89145f3d819fa
Алексей Кухаренко <druggist5059@gmail.com>
2020-02-22 14:53:44 +0300

    Добавляет неактивный режим, реализует активацию карты по кнопке. Добавляет валидацию для форм количества комнат и гостей

--- js/main.js
+++ js/main.js
@@ -1,17 +1,56 @@
 'use strict';
 
 var APARTMENTS = ['palace', 'flat', 'house', 'bungalo'];
+var TypeOfHouse = {
+  flat: 'Квартира',
+  bungalo: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец'
+};
 var TIMES = ['12:00', '13:00', '14:00'];
 var PIN_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PIN_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+var WIDTH_MAIN_PIN = 65;
+var HEIGHT_MAIN_PIN = 65;
+var HEIGHT_SHARP_MAIN_POINT = 22;
+var SOURCE_X_COORDINATA_MAIN_PIN = 570;
+var SOURCE_Y_COORDINATA_MAIN_PIN = 375;
+
 var MAX_PIN = 8;
 var X_AXIS_OFFSET = 25;
 var Y_AXIS_OFFSET = 70;
 
+var ENTER_KEY = 'Enter';
+var LEFT_MOUSE_BUTTON = 0;
+
 var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+
+var activationButton = map.querySelector('.map__pin--main');
+
+var filterMapForm = document.querySelector('.map__filters');
+
+var filterMapSelect = filterMapForm.querySelectorAll('select');
+filterMapSelect.forEach(function (n) {
+  n.setAttribute('disabled', 'disabled');
+});
+
+var filterMapFieldset = filterMapForm.querySelectorAll('fieldset');
+filterMapFieldset.forEach(function (n) {
+  n.setAttribute('disabled', 'disabled');
+});
+
+var newNoticeForm = document.querySelector('.ad-form');
+var newNoticeFieldset = newNoticeForm.querySelectorAll('fieldset');
+newNoticeFieldset.forEach(function (n) {
+  n.setAttribute('disabled', 'disabled');
+});
+var addressInput = newNoticeForm.querySelector('#address');
+addressInput.value = (SOURCE_X_COORDINATA_MAIN_PIN + WIDTH_MAIN_PIN * 0.5)
++ ', '
++ (SOURCE_Y_COORDINATA_MAIN_PIN + HEIGHT_MAIN_PIN * 0.5);
 
 var similarListPin = document.querySelector('.map__pins');
 
@@ -59,7 +98,7 @@ var createAnnouncement = function () {
 
       offer: {
         title: 'Заголовок объявления',
-        address: coordinateX + ',' + coordinateY,
+        address: coordinateX + ', ' + coordinateY,
         price: getRandomNumberInRange(10000, 100000),
         type: getRandomElement(APARTMENTS),
         rooms: 4,
@@ -100,15 +139,6 @@ var renderPins = function () {
     fragment.appendChild(renderPin(announcements[j]));
   }
   return fragment;
-};
-
-similarListPin.appendChild(renderPins());
-
-var TypeOfHouse = {
-  flat: 'Квартира',
-  bungalo: 'Бунгало',
-  house: 'Дом',
-  palace: 'Дворец'
 };
 
 var renderAnnouncementCard = function (card) {
@@ -158,4 +188,81 @@ var renderAnnouncementCard = function (card) {
   return cardElement;
 };
 
-similarFilters.before(renderAnnouncementCard(announcements[0]));
+var activatesThePage = function () {
+  map.classList.remove('map--faded');
+  similarListPin.appendChild(renderPins());
+  similarFilters.before(renderAnnouncementCard(announcements[0]));
+
+  filterMapSelect.forEach(function (n) {
+    n.removeAttribute('disabled', 'disabled');
+  });
+
+  filterMapFieldset.forEach(function (n) {
+    n.removeAttribute('disabled', 'disabled');
+  });
+
+  newNoticeForm.classList.remove('ad-form--disabled');
+
+  newNoticeFieldset.forEach(function (n) {
+    n.removeAttribute('disabled', 'disabled');
+  });
+};
+
+var setSharpPointCoordinates = function () {
+  addressInput.value = (SOURCE_X_COORDINATA_MAIN_PIN + WIDTH_MAIN_PIN * 0.5)
+  + ', '
+  + (SOURCE_Y_COORDINATA_MAIN_PIN + HEIGHT_MAIN_PIN + HEIGHT_SHARP_MAIN_POINT);
+};
+
+activationButton.addEventListener('mousedown', function (evt) {
+  if (typeof evt === 'object') {
+    switch (evt.button) {
+      case LEFT_MOUSE_BUTTON:
+        activatesThePage();
+        setSharpPointCoordinates();
+        break;
+    }
+  }
+});
+
+activationButton.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    activatesThePage();
+    setSharpPointCoordinates();
+  }
+});
+
+var roomNumber = newNoticeForm.querySelector('#room_number');
+var capacity = newNoticeForm.querySelector('#capacity');
+for (var i = 0; i < capacity.length; i++) {
+  capacity.children[i].setAttribute('disabled', 'disabled');
+}
+capacity.children[2].removeAttribute('disabled', 'disabled');
+capacity.children[2].setAttribute('selected', 'selected');
+
+roomNumber.addEventListener('change', function () {
+  var currentVal = roomNumber.value;
+  for (var j = capacity.children.length - 2; j >= 0; j--) {
+    if (currentVal === '1') {
+      capacity.children[j].setAttribute('disabled', 'disabled');
+      capacity.children[2].removeAttribute('disabled', 'disabled');
+      capacity.children[2].setAttribute('selected', 'selected');
+      capacity.children[capacity.children.length - 1].removeAttribute('selected', 'selected');
+    } else if (currentVal === '2') {
+      capacity.children[j].removeAttribute('disabled', 'disabled');
+      capacity.children[0].setAttribute('disabled', 'disabled');
+      capacity.children[capacity.children.length - 1].setAttribute('disabled', 'disabled');
+      capacity.children[capacity.children.length - 1].removeAttribute('selected', 'selected');
+    } else if (currentVal === '3') {
+      capacity.children[0].setAttribute('disabled', 'disabled');
+      capacity.children[j].removeAttribute('disabled', 'disabled');
+      capacity.children[capacity.children.length - 1].setAttribute('disabled', 'disabled');
+      capacity.children[capacity.children.length - 1].removeAttribute('selected', 'selected');
+
+    } else if (currentVal === '100') {
+      capacity.children[j].setAttribute('disabled', 'disabled');
+      capacity.children[capacity.children.length - 1].removeAttribute('disabled', 'disabled');
+      capacity.children[capacity.children.length - 1].setAttribute('selected', 'selected');
+    }
+  }
+});
