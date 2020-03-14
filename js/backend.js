4716a3efafc926a93068056a6822be6860d09605
Алексей Кухаренко <druggist5059@gmail.com>
2020-03-14 16:10:36 +0300

    Добавляет обработчик отправки на данных на сервер

--- js/backend.js
+++ js/backend.js
@@ -2,7 +2,12 @@
 (function () {
 
   var ServerLinks = {
-    URL_GET: 'https://js.dump.academy/keksobooking/data'
+    URL_GET: 'https://js.dump.academy/keksobooking/data',
+    URL_POST: 'https://js.dump.academy/keksobooking'
+  };
+
+  var StatusCodes = {
+    OK: 200
   };
 
   var RESPONSE_TYPE = 'json';
@@ -20,8 +25,28 @@
     xhr.send();
   };
 
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = RESPONSE_TYPE;
+
+    xhr.open('POST', ServerLinks.URL_POST);
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCodes.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError();
+      }
+    });
+
+    xhr.send(data);
+  };
+
   window.backend = {
-    getData: load
+    getData: load,
+    sentData: save
   };
 
 })();
+
+
