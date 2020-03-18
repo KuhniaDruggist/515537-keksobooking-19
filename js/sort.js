e86b40f2c895e41f5e26ea78fe0477f64303454f
Алексей Кухаренко <druggist5059@gmail.com>
2020-03-18 20:01:51 +0300

    рефакторинг модуля

--- js/sort.js
+++ js/sort.js
@@ -1,22 +1,29 @@
 'use strict';
 (function () {
 
+  var HousingTypeFildValue = {
+    ANY: 'any'
+  };
+
   var filterList = document.querySelector('.map__filters');
   var housingType = filterList.querySelector('#housing-type');
 
+  var filterPins = function (announcements) {
+    var listByTypeOfHousing = announcements.filter(function (pin) {
+      if (housingType.value === HousingTypeFildValue.ANY) {
+        return pin.offer.type;
+      }
+      return pin.offer.type === housingType.value;
+    });
+    return listByTypeOfHousing;
+  };
+
   var filtersAnnouncement = function (announcements) {
+    window.pins.render(announcements);
     housingType.addEventListener('change', function () {
-
-      var listByTypeOfHousing = announcements.filter(function (pin) {
-        if (housingType.value === 'any') {
-          return pin.offer.type;
-        }
-        return pin.offer.type === housingType.value;
-      });
-
       window.pins.remove();
       window.card.remove();
-      window.pins.render(listByTypeOfHousing);
+      window.pins.render(filterPins(announcements));
     });
   };
 
